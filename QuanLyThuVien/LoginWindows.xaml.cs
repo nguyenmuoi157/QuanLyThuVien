3769d80425716b4c8e37b8802a2759fad2d52ddb
mười nguyễn <11152967@st.neu.edu.vn>
2018-04-01 11:40:48 +0700

    tao du an

---
+++ QuanLyThuVien/LoginWindows.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLyThuVien
+{
+    /// <summary>
+    /// Interaction logic for LoginWindows.xaml
+    /// </summary>
+    public partial class LoginWindows : Window
+    {
+        public LoginWindows()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ QuanLyThuVien/MainWindows.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLyThuVien
+{
+    /// <summary>
+    /// Interaction logic for MainWindows.xaml
+    /// </summary>
+    public partial class MainWindows : Window
+    {
+        public MainWindows()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+            MovieCusorMenu(index);
+
+            switch (index)
+            {
+                case 0:
+                    MainContent.Children.Clear();
+                    MainContent.Children.Add(new UserControlDaTaBroad());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void MovieCusorMenu(int index)
+        {
+            TransitionSlide.OnApplyTemplate();
+            GridSlider.Margin = new Thickness(0, 150 + (60 * index), 0, 0);
+        }
+    }
+}
